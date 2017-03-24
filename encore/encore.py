55cb3337734a139ebe850b0aff1ae5fb431b2916
Tom McClintock <tmcclintock@email.arizona.edu>
2017-03-23 17:02:30 -0700

    Updated some comments in encore.py

--- encore/encore.py
+++ encore/encore.py
@@ -1,73 +1,16 @@
 """
 The encore class.
-
-There is a unit test at the bottom of this file.
 """
 import os
 
 class encore(object):
     """The encore class. Used to analyze rockstar halo catalogs along with accompanying dark matter particle catalogs.
-    
     """
-
-    def OLD__init__(self,halopath='NOT INITIALIZED',dmpath="NOT INITIALIZED",
-                 randompath=None,DSdmpath=None,reducedhalopath=None,
-                 outpath="./",particle_mass=3e10,do_JK=False,ndivs=2,DSF=1000):
-        """Create the encore object.
-
-        Args:
-            halopath (string; optional): TODO
-            dmpath (string; optional): TODO
-            randompath (string; optional): TODO
-            DSdmpath (string; optional): TODO
-            reducedhalopath (string; optional): TODO
-            outpath (string; optional): Path for all output files; default is ./.
-            particle_mass (float; optional): Mass of dark matter particles; default is 3e10 Msun/h; note that units are arbitrary.
-            do_JK (boolean; optional): Compute jackknifes for all quantities, including jackknifing the DM and halo catalogs; default is False.
-            ndivs (int; optional): TODO
-            DSF (int; optional): TODO
-
-        """
-        self.outpath = outpath #Path to the output
-        self.particle_mass = particle_mass #Msun/h
-        self.halopath = halopath #Unreduced halos
-        self.dmpath = dmpath #Undown sampled dark matter
-        self.do_JK = do_JK #Boolean to do jackknifing
-        self.ndivs = ndivs #Number of JK divisions in a dimension
-        self.DSF = DSF #Down sampling factor
-        if DSdmpath is not None: self.DSdmpath = DSdmpath
-        else: self.DSdmpath = self.outpath
-        if os.path.exists(self.DSdmpath+"/down_sampled_dm/down_sampled_dm_DSF%d"%DSF):
-            print "Down sampled DM catalog found in %s"%self.DSdmpath
-            print "\tDown sampling complete."
-            self.down_sampled = True
-        else: self.down_sampled = False
-        if randompath is not None: self.randompath = randompath
-        else: self.randompath = self.outpath
-        if os.path.exists(self.randompath+"/randoms/"):
-            print "Random catalogs found in %s"%self.randompath
-            print "\tFor new cats: create_random_catalogs(...,recreate=True)."
-            self.have_randoms = True
-        else: self.have_randoms = False
-        if reducedhalopath is not None: self.reducedhalopath = reducedhalopath
-        else: self.reducedhalopath = self.outpath
-        if os.path.exists(self.reducedhalopath+"/reduced_halo_cats/reduced_halo_cat.txt"):
-            print "Reduced halo catalog found at %s"%self.reducedhalopath
-            print "\tFor new cats: reduce_halo_catalogs(...,recreate=True)."
-            self.have_reducedhalos = True
-        else: self.have_reducedhalos = False
-        #Make a path for the info files
-        os.system("mkdir -p %s"%outpath+"/info_files/")
-
     def __init__(self,**kwargs):
         """Create a rockstar object.
-
-        Note: if not set, the output path is set to './'.
-
         """
         for key, value in kwargs.items():
             setattr(self, key, value)
-        if not hasattr(self,"outpath"): self.outpath = "./"
 
     def __str__(self):
         outstr = "Rockstar encore with:"
@@ -77,7 +20,7 @@ class encore(object):
 
     def reduce_halo_catalogs(self,recreate=False):
         """
-        Reduce the halo catalog.
+        Reduce the halo catalog by removing halos that are too small (<200 particles).
 
         Args:
             recreate (bool): Flag to re-reduce the rockstar catalog, even if it's already reduced; default False.
@@ -136,6 +79,8 @@ class encore(object):
 
     def compute_mass_function(self):
         """Computes the halo mass function.
+
+        Note: jkcatalog must be a formatted string that takes a single integer representing which jackknife region it is.
         """
         import mass_function
         cat = getattr(self,"catalog")
